1b70145a3fe18f2dfb52e9144476a78886f09fc3
Nobel Davis <75487166+nobeldavis@users.noreply.github.com>
2021-05-18 17:27:38 +0530

    Update script.js

--- 02-Infinite-Scroller/script.js
+++ 02-Infinite-Scroller/script.js
@@ -44,7 +44,7 @@ function displayPhotos(){
   });
 }
 /* Unsplash API */
-const count = 30;
+const count = 29;
 const apiKey = 'uUbaA6Q7JBUn-9aeJgQDOTe88_xzyG2_H7SAjjpKi7k';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
